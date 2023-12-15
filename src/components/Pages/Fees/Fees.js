4c6f85f85afcb5d32f7a20cb96641331b716a365
AlexanderBraatz <alex.braatz.g@gmail.com>
2023-12-15 15:02:27 +0000

    feat: implement responsive design for fees page

--- src/components/Pages/Fees/Fees.js
+++ src/components/Pages/Fees/Fees.js
@@ -1,34 +1,49 @@
 import styled from 'styled-components';
-import { ReactComponent as CostExpensesGraph } from '../../../assets/images/CostExpensesGraph.svg';
+import FeesImage from '../../../assets/images/Feediagram2.png';
 
 export default function Fees() {
 	return (
-		<Container>
-			<Heading>Fees</Heading>
-			<Content>
-				<Text>
-					Our fees are negotiable, though the chart on the right shows an
-					estimate of how much we charge for a full service for each client
-					based on the cost of the works. <br /> <br />
-					According to this chart, the maximum fee for a project that costs
-					£30,000 would be around £4,500, while the maximum fee for a project
-					that costs £100,000 would be around £11,000. <br /> <br />
-					We reserve the right to adjust your fee due to unforeseen
-					complexities. If you only need a partial service, we can adjust the
-					fee accordingly based on an assessment of the building cost.
-				</Text>
-				<StyledCostExpensesGraph />
-			</Content>
-		</Container>
+		<Background>
+			<Container>
+				<Heading>Fees</Heading>
+				<Content>
+					<Text>
+						Our fees are negotiable, though the chart on the right gives an
+						estimate of how much we charge for a full service for each client
+						based on the cost of the works. <br /> <br />
+						According to this chart, the fee for a project that costs £10,000
+						would be about £1,000, while the fee for a project that costs
+						£100,000 would be around £5,000. <br /> <br />
+						It may be necessary to adjust your fee due to unforeseen
+						complexities. If you only need a partial service, we will adjust the
+						fee based on an assessment of the project.
+					</Text>
+					<GraphContainer>
+						{/* <StyledCostExpensesGraph /> */}
+						<Feediagram2Image
+							src={FeesImage}
+							alt="Graph displaying the fees and "
+						/>
+					</GraphContainer>
+				</Content>
+			</Container>
+		</Background>
 	);
 }
-
+const Background = styled.div`
+	background-color: ${props => props.theme.desktop.dark_1};
+	min-height: 100vh;
+	width: 100%;
+`;
 const Container = styled.div`
-	width: 83.3rem;
+	max-width: 94.8rem;
 	margin: auto;
-	margin-top: 5.35rem;
 	display: absolute;
-	transform: translateX(-3rem);
+	padding: 4.8rem 3rem;
+	height: fit-content;
+	@media (max-width: 843px) {
+		padding: 2.4rem 1.6rem;
+	}
 `;
 const Heading = styled.h1`
 	color: ${props => props.theme.desktop.grey_5};
@@ -37,19 +52,35 @@ const Heading = styled.h1`
 	font-weight: 400;
 	letter-spacing: -0.048rem;
 	text-align: left;
+	margin: auto;
 	margin-bottom: 2.8rem;
+	@media (max-width: 965px) {
+		max-width: 48rem;
+	}
 `;
 const Content = styled.div`
 	display: flex;
 	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	gap: 4rem;
+	@media (max-width: 965px) {
+		flex-direction: column;
+	}
 `;
 const Text = styled.p`
 	color: ${props => props.theme.desktop.grey_5};
 	font-family: 'Body', Arial, Serif;
 	font-size: 1.6rem;
 	text-align: left;
-	width: 40.8rem;
+	max-width: 32.4rem;
+	@media (max-width: 965px) {
+		max-width: 48rem;
+	}
 `;
-const StyledCostExpensesGraph = styled(CostExpensesGraph)`
-	transform: translateY(-0.5rem);
+const GraphContainer = styled.div``;
+
+const Feediagram2Image = styled.img`
+	width: 100%;
+	height: 100%;
 `;
